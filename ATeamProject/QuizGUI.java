ee9fa32e1aec312755c3cbb3a583b4cf16203c4a
Alex Moon <ajmoon2@wisc.edu>
2019-04-23 12:46:27 -0500

    Added a quizGUI interface to coordinate work

---
+++ ATeamProject/QuizGUI.java
@@ -0,0 +1,27 @@
+package ATeamProject;
+
+import javafx.stage.Stage;
+
+public interface QuizGUI {
+
+	/**
+	 * The start page of the application.
+	 * Questions can be created here and JSON Files can be loaded
+	 * @param primaryStage The primary stage
+	 */
+	public void mainScreen(Stage primaryStage);
+	
+	/**
+	 * The second page, where topics are chosen from the list
+	 * of available topics to start generating a quiz.
+	 * @param primaryStage The primary stage
+	 */
+	public void topicChoosingPage(Stage primaryStage);
+	
+	/**
+	 * The page where the quiz is actually taken.
+	 * @param primaryStage The primary stage
+	 */
+	public void takingQuizPage(Stage primaryStage);
+	
+}
