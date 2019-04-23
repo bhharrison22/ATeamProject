2a367b22dcec3a83b110f81682e3a4091fca29a1
Alex Moon <ajmoon2@wisc.edu>
2019-04-23 13:19:49 -0500

    Added very basic topic methods for my use

--- ATeamProject/Topic.java
+++ ATeamProject/Topic.java
@@ -3,4 +3,14 @@ package ATeamProject;
 
 public class Topic {
 
+	private String name;
+	
+	public Topic(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
 }
