fd1f690b039364b7ed6868531abf7691a2442a37
Alex Moon <ajmoon2@wisc.edu>
2019-04-23 12:23:15 -0500

    Added QuizADT

---
+++ QuizADT.java
@@ -0,0 +1,52 @@
+package ATeamProject;
+
+/**
+ * A basic quiz ADT that uses all of the methods from the design doc
+ * @author amoon
+ */
+public interface QuizADT {
+
+	/**
+	 * Adds a question to the question database
+	 * @param question The text of the question
+	 * @param answer The answer of the question
+	 * @param options Question options
+	 * @param topic The topic of the question
+	 */
+	public void addQuestion(String questionText, String answer, String[] options, String topic);
+	
+	/**
+	 * Loads questions into the program from a JSON file
+	 * @param JSONfilePath The file path of the questions
+	 */
+	public void loadQuestions(String JSONfilePath);
+	
+	/**
+	 * Saves all added questions into a JSON file
+	 */
+	public void save();
+	
+	/**
+	 * Autogenerates a list of questions from a set of topics
+	 * @param topics The set of topics
+	 * @param numQuestions The number of questions to be generated
+	 * @return The list of questions that have been randomly generated
+	 */
+	public Question[] generateQuizQuestions(Topic[] topics, int numQuestions);
+	
+	/**
+	 * Generates a quiz from a set of questions
+	 * @param quizQuestions The set of questions
+	 * @return A list of strings that represent the questions
+	 */
+	public String[] generateQuiz(Question[] quizQuestions);
+	
+	/**
+	 * Grades a set of answers based on a set of questions
+	 * @param quizQuestions The set of questions
+	 * @param answers The set of answers (pre: is same length as quizQuestions)
+	 * @return A string that can be read to see the grade.
+	 */
+	public String grade(Question[] quizQuestions, String[] answers);
+	
+}
