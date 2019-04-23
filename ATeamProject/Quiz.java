f98cc0170e0a4156e9ed06b8eb38b3b1a29b0e7f
Alex Moon <ajmoon2@wisc.edu>
2019-04-23 12:33:41 -0500

    Added JavaFX to the build path

--- ATeamProject/Quiz.java
+++ ATeamProject/Quiz.java
@@ -1,10 +1,46 @@
 package ATeamProject;
 
-public class Quiz implements QuizADT extends Application {
+public class Quiz implements QuizADT {
 
-  public static void main(String[] args) {
-    // TODO Auto-generated method stub
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 
-  }
+	}
+
+	@Override
+	public void addQuestion(String questionText, String answer, String[] options, String topic) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void loadQuestions(String JSONfilePath) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void save() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Question[] generateQuizQuestions(Topic[] topics, int numQuestions) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String[] generateQuiz(Question[] quizQuestions) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String grade(Question[] quizQuestions, String[] answers) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
