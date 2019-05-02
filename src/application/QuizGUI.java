af093657d3d5a5e91e299efc484a38a9ca2f162d
bhharrison22 <bhharrison@wisc.edu>
2019-05-01 23:58:39 -0500

    Formatted QuizGUI.java to CS300 style guide

--- src/application/QuizGUI.java
+++ src/application/QuizGUI.java
@@ -47,14 +47,14 @@ public class QuizGUI extends Application implements QuizGUIADT {
     Label welcome = new Label("WELCOME TO QUIZ GENERATOR");
     Label numQues = new Label("Number of Questions in Quiz: ");
     this.counter.setText(this.quiz.numQuestions() + "");
-    
+
     GridPane grid = new GridPane();
-    grid.setPadding(new Insets(10,10,10,10));
-    grid.setMinSize(300,300);
+    grid.setPadding(new Insets(10, 10, 10, 10));
+    grid.setMinSize(300, 300);
     grid.setVgap(20);
     grid.setHgap(20);
     grid.setAlignment(Pos.CENTER);
-    
+
     Button add = new Button("Add Question");
     add.setMaxWidth(150);
     Button load = new Button("Load Question");
@@ -64,33 +64,33 @@ public class QuizGUI extends Application implements QuizGUIADT {
     Button next = new Button("Make Quiz");
     next.setMaxWidth(150);
 
-    add.setOnAction(e-> addQuestionPage(primaryStage));
-    load.setOnAction(e-> loadQuestionPage(primaryStage));
-    save.setOnAction(e-> quiz.save());
-    next.setOnAction(e-> topicChoosingPage(primaryStage));
-    
+    add.setOnAction(e -> addQuestionPage(primaryStage));
+    load.setOnAction(e -> loadQuestionPage(primaryStage));
+    save.setOnAction(e -> quiz.save());
+    next.setOnAction(e -> topicChoosingPage(primaryStage));
+
     grid.add(add, 0, 2);
     grid.add(load, 1, 2);
     grid.add(save, 0, 3);
     grid.add(next, 1, 3);
- 
+
     HBox welcomeLabel = new HBox();
-    welcomeLabel.setPadding(new Insets(50,0,0,0));
+    welcomeLabel.setPadding(new Insets(50, 0, 0, 0));
     welcomeLabel.setAlignment(Pos.CENTER);
     welcomeLabel.getChildren().add(welcome);
-    
+
     VBox root = new VBox();
     root.getChildren().addAll(welcomeLabel, grid);
     HBox questionCounter = new HBox();
     questionCounter.getChildren().addAll(numQues, this.counter);
-    
-    root.getChildren().add(questionCounter);
-    
 
-    
-    Scene scene = new Scene(root); 
+    root.getChildren().add(questionCounter);
+
+
+
+    Scene scene = new Scene(root);
     primaryStage.setScene(scene);
-    primaryStage.show();    
+    primaryStage.show();
   }
 
   @Override
@@ -224,119 +224,122 @@ public class QuizGUI extends Application implements QuizGUIADT {
 
   @Override
   public void takingQuizPage(Stage primaryStage, Question[] questions) {
-	  //TODO test
-//	  String[] choices = {"A is a subset of B, but not a proper subset of B", "A is a proper subset of B", "A is a superset of B, but not a proper superset of B",
-//			  "A is a proper superset of B", "A is the complement of B"};
-//	  
-//	  Question A = new Question("Which statement is TRUE regarding sets A and B?", "A is a proper superset of B", null , choices, "set");
-//	  
-//	  String[] choices1 = {"when the problem size is small", "when the problem size is large"};
-//	  
-//	  Question B = new Question("When two algorithms have different big-O time complexity, the constants and low-order terms only matter ________. ",
-//			  "when the problem size is small", "application/bucky.png", choices1, "performance");
-//	  
-//	  Question[] questions1 = {A,B};
-//	  
-	  int numCorrect = 0;
-//	  
-//	  for(Question q: questions1) {
-//			  if(renderQuestion(primaryStage, q)) {
-//				  numCorrect++;
-//			  } else {
-//			  }
-//	  }
-	  
-	 //Render final results page
-	 //Back to Homescreen Button
-	  for (Question q: questions) {
-	    //Possible for there to be a tail of nulls
-	    if (q == null) {
-	      break;
-	    } else {
-	      if (renderQuestion(primaryStage, q)) {
-	        numCorrect++;
-	      }
-	    }
-	  }
+    // TODO test
+    // String[] choices = {"A is a subset of B, but not a proper subset of B", "A is a proper subset
+    // of B", "A is a superset of B, but not a proper superset of B",
+    // "A is a proper superset of B", "A is the complement of B"};
+    //
+    // Question A = new Question("Which statement is TRUE regarding sets A and B?", "A is a proper
+    // superset of B", null , choices, "set");
+    //
+    // String[] choices1 = {"when the problem size is small", "when the problem size is large"};
+    //
+    // Question B = new Question("When two algorithms have different big-O time complexity, the
+    // constants and low-order terms only matter ________. ",
+    // "when the problem size is small", "application/bucky.png", choices1, "performance");
+    //
+    // Question[] questions1 = {A,B};
+    //
+    int numCorrect = 0;
+    //
+    // for(Question q: questions1) {
+    // if(renderQuestion(primaryStage, q)) {
+    // numCorrect++;
+    // } else {
+    // }
+    // }
 
-		 //Render final results page
-		 //Back to Homescreen Button
-	  }
-  private Boolean renderQuestion(Stage primaryStage, Question q) {
-	  Stage secondary = new Stage();
-	  boolean result = false;
-
-      VBox layout = new VBox();
-      Scene scene = new Scene(layout, 700, 700);
-      
-      
-      Label title = new Label("Quiz");
-      title.setFont(Font.font(50));
-      layout.getChildren().add(title);
-      layout.setAlignment(Pos.BASELINE_CENTER);
-      
-      Label question = new Label(q.getText()); 
-      question.setFont(Font.font(10));
-      layout.getChildren().add(question);
-      layout.setAlignment(Pos.BASELINE_CENTER);
-      
-      try {
-        if(q.getImagePath() != null) {
-          ImageView img = new ImageView(q.getImagePath()); 
-          img.setFitHeight(100);
-          img.setFitWidth(100);
-          layout.getChildren().add(img);
-          layout.setAlignment(Pos.BASELINE_CENTER);
-        }
-      } catch (IllegalArgumentException e) {
-        System.out.println("Bad image path");
-      }
-      
-      for(String s: q.getChoiceArray()) {
-    	  Label choice = new Label(s); 
-          choice.setFont(Font.font(10));
-          layout.getChildren().add(choice);
-          layout.setAlignment(Pos.BASELINE_CENTER);
-      }
-      
-      TextField answer = new TextField();
-      
-      answer.setOnAction( e -> {
-      boolean correct = checkAnswer(result, answer.getText(), q.getAnswer());
-      
-      if(correct) {
-    	  layout.getChildren().add(new Label("Correct!"));
+    // Render final results page
+    // Back to Homescreen Button
+    for (Question q : questions) {
+      // Possible for there to be a tail of nulls
+      if (q == null) {
+        break;
       } else {
-    	  layout.getChildren().add(new Label("Incorrect!"));
-      }	 
+        if (renderQuestion(primaryStage, q)) {
+          numCorrect++;
+        }
+      }
+    }
+
+    // Render final results page
+    // Back to Homescreen Button
+  }
+
+  private Boolean renderQuestion(Stage primaryStage, Question q) {
+    Stage secondary = new Stage();
+    boolean result = false;
+
+    VBox layout = new VBox();
+    Scene scene = new Scene(layout, 700, 700);
+
+
+    Label title = new Label("Quiz");
+    title.setFont(Font.font(50));
+    layout.getChildren().add(title);
+    layout.setAlignment(Pos.BASELINE_CENTER);
+
+    Label question = new Label(q.getText());
+    question.setFont(Font.font(10));
+    layout.getChildren().add(question);
+    layout.setAlignment(Pos.BASELINE_CENTER);
+
+    try {
+      if (q.getImagePath() != null) {
+        ImageView img = new ImageView(q.getImagePath());
+        img.setFitHeight(100);
+        img.setFitWidth(100);
+        layout.getChildren().add(img);
+        layout.setAlignment(Pos.BASELINE_CENTER);
+      }
+    } catch (IllegalArgumentException e) {
+      System.out.println("Bad image path");
+    }
+
+    for (String s : q.getChoiceArray()) {
+      Label choice = new Label(s);
+      choice.setFont(Font.font(10));
+      layout.getChildren().add(choice);
+      layout.setAlignment(Pos.BASELINE_CENTER);
+    }
+
+    TextField answer = new TextField();
+
+    answer.setOnAction(e -> {
+      boolean correct = checkAnswer(result, answer.getText(), q.getAnswer());
+
+      if (correct) {
+        layout.getChildren().add(new Label("Correct!"));
+      } else {
+        layout.getChildren().add(new Label("Incorrect!"));
+      }
       secondary.close();
-      });
-     
-      layout.getChildren().add(answer);
-      
-      secondary.setScene(scene);
-      secondary.showAndWait();
-      
-	  // when NEXT -> close that screen and return result
-      
-      
-      return result;
-}
+    });
 
-  
-  
+    layout.getChildren().add(answer);
 
-private boolean checkAnswer(boolean result, String input, String answer) {
-	if(input.equals(answer)) {
-		result = true;
-		return true;
-	} else {
-		result = false;
-		return false;
-	}	
-}
+    secondary.setScene(scene);
+    secondary.showAndWait();
 
-@Override
+    // when NEXT -> close that screen and return result
+
+
+    return result;
+  }
+
+
+
+  private boolean checkAnswer(boolean result, String input, String answer) {
+    if (input.equals(answer)) {
+      result = true;
+      return true;
+    } else {
+      result = false;
+      return false;
+    }
+  }
+
+  @Override
   public void start(Stage primaryStage) throws Exception {
     this.quiz = new Quiz();
     try {
@@ -358,7 +361,7 @@ private boolean checkAnswer(boolean result, String input, String answer) {
     launch(args);
 
   }
-  
+
   public void addQuestionPage(Stage primaryStage) {
     VBox mainBox = new VBox();
     VBox choices = new VBox();
@@ -381,7 +384,7 @@ private boolean checkAnswer(boolean result, String input, String answer) {
     content.setPromptText("Enter the question here");
     content.setMinHeight(50);
     content.setWrapText(true);
-    
+
     TextField image = new TextField();
     image.setPromptText("Enter the path for the image");
     TextField choiceA = new TextField();
@@ -394,7 +397,7 @@ private boolean checkAnswer(boolean result, String input, String answer) {
     choiceD.setPromptText("Enter choice D here");
     TextField choiceE = new TextField();
     choiceE.setPromptText("Enter choice E here");
-    
+
     RadioButton rb1 = new RadioButton("A");
     RadioButton rb2 = new RadioButton("B");
     RadioButton rb3 = new RadioButton("C");
@@ -415,29 +418,24 @@ private boolean checkAnswer(boolean result, String input, String answer) {
     Button backButton = new Button("Back");
     backButton.setOnAction(e -> mainScreen(primaryStage));
     Button addButton = new Button("Add");
-    EventHandler<MouseEvent> addEventHandler = new EventHandler<MouseEvent>() { 
-      @Override 
-      public void handle(MouseEvent e) { 
+    EventHandler<MouseEvent> addEventHandler = new EventHandler<MouseEvent>() {
+      @Override
+      public void handle(MouseEvent e) {
         mainScreen(primaryStage);
         String answer = null;
-        if(rb1.isSelected() == true) 
-        {
+        if (rb1.isSelected() == true) {
           answer = choiceA.getText();
         }
-        if(rb2.isSelected() == true) 
-        {
+        if (rb2.isSelected() == true) {
           answer = choiceB.getText();
         }
-        if(rb3.isSelected() == true) 
-        {
+        if (rb3.isSelected() == true) {
           answer = choiceC.getText();
         }
-        if(rb4.isSelected() == true) 
-        {
+        if (rb4.isSelected() == true) {
           answer = choiceD.getText();
         }
-        if(rb5.isSelected() == true) 
-        {
+        if (rb5.isSelected() == true) {
           answer = choiceE.getText();
         }
         String[] options = new String[5];
@@ -448,10 +446,9 @@ private boolean checkAnswer(boolean result, String input, String answer) {
         options[4] = choiceE.getText();
         String topicText = topic.getText();
         String imageText = image.getText();
-        quiz.addQuestion(content.getText(), answer, options, topicText,
-            imageText);
-      } 
-    };   
+        quiz.addQuestion(content.getText(), answer, options, topicText, imageText);
+      }
+    };
     addButton.addEventFilter(MouseEvent.MOUSE_CLICKED, addEventHandler);
     choices.getChildren().add(choiceA);
     choices.getChildren().add(choiceB);
@@ -496,8 +493,7 @@ private boolean checkAnswer(boolean result, String input, String answer) {
       if (loadQuestion((JSONFile.getText() + ".json"))) {
         resultLabel.setTextFill(Color.web("#0000FF"));
         resultLabel.setText("Questions Added!");
-      }
-      else {
+      } else {
         resultLabel.setTextFill(Color.web("#FF0000"));
         resultLabel.setText("Addition failed");
       }
@@ -521,7 +517,7 @@ private boolean checkAnswer(boolean result, String input, String answer) {
     primaryStage.setScene(scene);
     primaryStage.show();
   }
-  
+
   private boolean loadQuestion(String JSONfilePath) {
     try {
       quiz.loadQuestions(JSONfilePath);
