4438f66786bf2541ef044cc1f584d3ffb081bdbb
bhharrison22 <bhharrison@wisc.edu>
2019-05-02 09:41:11 -0500

    documented and formatted Question and Topic classes

--- src/application/Question.java
+++ src/application/Question.java
@@ -8,23 +8,37 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
+/**
+ * Class which contains a Question and all necessary information required to generate a quiz.
+ *
+ */
 public class Question {
   // Fields
   private String text;
   private String topic;
   private String imagePath;
   private String[] choiceArray;
-  private String answer; // index of choiceArray the the correct answer
-  
-  public Question(String text, String answer, String imagePath, String[] choiceArray,  String topic) {
+  private String answer;
+
+  /**
+   * Main constuctor for Question
+   * 
+   * @param text        is the text of the Question
+   * @param answer      is the answer of the Question
+   * @param imagePath   is the path to the image of the Question
+   * @param choiceArray is an array of choices, one of which is the answer
+   * @param topic       is the topic of the Question
+   */
+  public Question(String text, String answer, String imagePath, String[] choiceArray,
+      String topic) {
     this.text = text;
     this.topic = topic;
     this.imagePath = imagePath;
     this.choiceArray = choiceArray;
     this.answer = answer;
   }
-  
-  // Getter Methods: 
+
+  // Getter Methods:
 
   public String getText() {
     return text;
@@ -37,7 +51,7 @@ public class Question {
   public String getImagePath() {
     return imagePath;
   }
-  
+
   public String getAnswer() {
     return answer;
   }
@@ -45,5 +59,4 @@ public class Question {
   public String[] getChoiceArray() {
     return choiceArray;
   }
-  
 }
